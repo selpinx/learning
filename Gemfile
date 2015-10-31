64f06506e1edb54e8110752c28514aaeebfeb4fe
selpinx <jablonowskipiotr@gmail.com>
2015-10-31 14:44:24 +0100

    First commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'page-object', '1.1.0'
+gem 'selenium-webdriver', '2.47.1'
+gem 'watir-webdriver', '0.8.0'
+gem 'rspec', '3.3.0'
+
+gem 'pry'
---
+++ google.rb
@@ -0,0 +1,26 @@
+require 'watir-webdriver'
+require 'pry'
+
+
+browser = Watir::Browser.new :firefox
+browser.goto "http://translate.google.com/"
+ 
+# binding.pry
+language_button = browser.div(:id => "gt-sl-gms")
+language_button.when_present.click
+ 
+menu = browser.div(:id => "gt-sl-gms-menu")
+menu.when_present.div(
+  :class => /goog-menu/,
+  :text => "norweski"
+).div.click
+ 
+puts language_button.text
+ 
+browser.text_field(:id => "source").set("ost")
+ 
+result_box = browser.span(:id => "result_box")
+browser.wait_until { result_box.text.length > 0 }
+ 
+puts result_box.text
+browser.close
