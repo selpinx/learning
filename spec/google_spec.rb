57539255d2595299758c71c931297260b055eab5
selpinx <jablonowskipiotr@gmail.com>
2015-11-01 18:43:23 +0100

    Refactoring testing code for using page_object pattern

--- spec/google_spec.rb
+++ spec/google_spec.rb
@@ -1,31 +1,30 @@
 require 'spec_helper'
 
 describe "Google translate" do
+	before(:all) do
+		@language = "niemiecki"
+	end
+
 	it "Go to google translate page" do
-		@browser.goto "http://translate.google.com/"		
+		visit(TranslateGooglePage)
 	end
 	it "Expand language switcher listbox" do
-		$language_button = @browser.div(:id => "gt-sl-gms")
-		$language_button.when_present.click
+		on(TranslateGooglePage).language_switcher_element.when_present.click
 	end
-	it "Choose language norweski" do
-		menu = @browser.div(:id => "gt-sl-gms-menu")
-		menu.when_present.div(
-  		:class => /goog-menu/,
-  		:text => "norweski"
-		).div.click
+	it "Choose language" do
+		puts "Select #{@language} language"
+		on(TranslateGooglePage).select_language(@language)
 	end
 	it "Display text from language switcher" do
-		puts $language_button.text
+		puts on(TranslateGooglePage).language_switcher_element.text
 	end
 	it "Set text 'ost' in source language text field" do
-		@browser.textarea(:id => "source").set("ost")
+		on(TranslateGooglePage).source_text_field = "ost"
 	end
 	it "Wait until translated text appears results box" do
-		$result_box = @browser.span(:id => "result_box")
-		@browser.wait_until { $result_box.text.length > 0 }
+		on(TranslateGooglePage).wait_until { on(TranslateGooglePage).result_box_element.text.length > 0 }
 	end
 	it "Display text from results box" do
-		puts $result_box.text
+		puts on(TranslateGooglePage).result_box_element.text
 	end
 end
